ebfa064fca9f36cdc55d690b470b54988f88ea99
davidquintero11 <xdavidsimon@gmail.com>
2026-02-19 23:05:06 -0400

    Buscador de peliculas con react y API

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BuscadorPeliculas } from './BuscadorPeliculas.jsx'
+import './styles/buscador.css'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BuscadorPeliculas/>
+  </StrictMode>,
+)
